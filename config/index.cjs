3d82608c5a2148ec8319dd7c40be83c7e00910e3
HAN101259 <113027498+HAN101259@users.noreply.github.com>
2023-03-28 16:53:39 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -22,7 +22,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '思姐',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU21293TZMSPZdr5XDNmfaNy8w2doyZWBHMzAW2E',
+      id: 'eaf588cd498048c98316a8fb4f45edc5',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -54,7 +54,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU21293TZMSPZdr5XDNmfaNy8w2doyZWBHMzAW2E',
+      id: 'eaf588cd498048c98316a8fb4f45edc5',
     }
   ],
 
